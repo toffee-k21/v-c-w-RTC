a98ac2dde98600786b89d5ac5e414f9c37fe45fd
Taufiq <taufiq9.2004@gmail.com>
2024-05-03 19:13:32 +0530

    socket initialize

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,22 @@
 const express = require("express");
+const { Server } = require("socket.io");
+const bodyParser = require("body-parser")
+const io = new Server();
 
 const app = express();
+
+app.use(bodyParser.json());
+
+const emailToSocketMapping = new Map();
+
+io.on("connection", socket =>{//connection toh predefined hogi
+    socket.on("room-join",(data)=>{
+        const {roomId, emailId} = data
+        emailToSocketMapping.set(emailId,socket.id)
+        socket.join(roomId)
+        socket.broadcast.to(roomId).emit("user-joined", {emailId})
+    })
+})
 
 app.get("/",(req,res)=>{
     res.send("hello")
@@ -9,3 +25,4 @@ app.get("/",(req,res)=>{
 app.listen(8000, ( )=>{
     console.log("server started !")
 })
+io.listen(8001)
