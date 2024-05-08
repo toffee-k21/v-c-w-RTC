18d1b0aec9831166aeea37d884dae34cdbe8c124
Taufiq <taufiq9.2004@gmail.com>
2024-05-08 17:31:34 +0530

    nA

--- frontend/src/pages/Room.jsx
+++ frontend/src/pages/Room.jsx
@@ -5,32 +5,36 @@ import { usePeer } from '../utils/PeerContext'
 
 const Room = () => {
 const{socket} = useSocket()
-const {Peer, createOffer, createAnswer, setRemoteAns, sendStream } = usePeer()
+const {Peer, createOffer, createAnswer, setRemoteAns, sendStream, remoteStream } = usePeer()
 const [myStream,setMyStream] = useState(null)
+const [ remoteEmailId, setRemoteEmailId ] = useState(null)
+// const [remoteStream,setRemoteStream] = useState(null)
 
 const handleRoomJoin = useCallback( async ({emailId} )=>{
 console.log("joinded",emailId)
 const offer = await createOffer();
 socket.emit("call-user",{offer,emailId})
+setRemoteEmailId(emailId);
 },[socket,createOffer])//calllback for performance issue na aaye
 
 const handleIncomingCall = useCallback(async({emailId,offer} )=>{
 console.log("a incoming call from",emailId,offer)
 const ans = await createAnswer(offer);
 socket.emit("call-accepted", {emailId: emailId, ans});
+setRemoteEmailId(emailId)
 },[createAnswer,socket])
 
-const handleCallAccepted = async (data)=>{
+const handleCallAccepted = useCallback (async (data)=>{
 const { ans } = data;
 console.log("call got accepted", ans)
-await setRemoteAns(ans)
-}
+await setRemoteAns(ans);
+
+},[setRemoteAns])
 
 const getUserMediaStream = useCallback(async ( )=>{
 const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true});
-sendStream(stream)
 setMyStream(stream)
-},[sendStream])
+},[])
 
     useEffect(()=>{
 socket.on("user-joined", handleRoomJoin)
@@ -47,10 +51,25 @@ return ( )=>{socket.off("user-joined", handleRoomJoin)
       getUserMediaStream();
       },[getUserMediaStream])
 
+      
+    const handleNegosiation = useCallback(()=>{
+      const localOffer = Peer.localDescription;
+      socket.emit("call-user", { email: remoteEmailId , offer: localOffer});
+  } , [Peer.localDescription, remoteEmailId, socket])
+
+  useEffect(()=>{
+    Peer.addEventListener('negotiationneeded',handleNegosiation)
+    return ()=>{ 
+      Peer.removeEventListener('negotiationneeded',handleNegosiation)
+  }
+  },[])
+
   return (
     <div>
-      <ReactPlayer url ={myStream} playing/>
-
+      <div>your r connected to : {remoteEmailId}</div>
+<button onClick={()=>sendStream(myStream)}>send my stream</button>
+      <ReactPlayer url ={myStream} playing muted/>
+      <ReactPlayer url ={remoteStream} playing/>
     </div>
   )
 }
--- frontend/src/utils/PeerContext.jsx
+++ frontend/src/utils/PeerContext.jsx
@@ -1,51 +1,75 @@
-import { createContext, useContext, useMemo } from "react";
+import { createContext, useCallback, useContext, useEffect, useMemo, useState } from "react";
 
-const PeerContext = createContext((null));
+const PeerContext = createContext(null);
 
-export const usePeer =()=>{
-  return  useContext(PeerContext)
+export const usePeer = () => {
+  return useContext(PeerContext);
 };
 
-export const PeerProvider = (props)=>{
-    //RTCPeerConnection ye basically SDP(info that is public ip address) la kr deta hai user ka
-    const Peer = useMemo(()=>new RTCPeerConnection({
-        iceServers:[
-            {
-                urls:[
-                    "stun:stun.l.google.com:19302",
-                    "stun:global.stun.twilio.com:3478"
-                ]
-            }
+export const PeerProvider = (props) => {
+  //RTCPeerConnection ye basically SDP(info that is public ip address) la kr deta hai user ka
 
-        ]
-    }),[])//this return SDP(info about User Computer)
+  const [ remoteStream, setRemoteStream ] = useState(null);
+  const Peer = useMemo(
+    () =>
+      new RTCPeerConnection({
+        iceServers: [
+          {
+            urls: [
+              "stun:stun.l.google.com:19302",
+              "stun:global.stun.twilio.com:3478",
+            ],
+          },
+        ],
+      }),
+    []
+  ); //this return SDP(info about User Computer)
 
-    const createOffer = async ( ) =>{
-        const offer = await Peer.createOffer();
-        await Peer.setLocalDescription(offer);
-        return offer
-        //this funtion will create offer and retun it and also store its info
+  const createOffer = async () => {
+    const offer = await Peer.createOffer();
+    await Peer.setLocalDescription(offer);
+    return offer;
+    //this funtion will create offer and retun it and also store its info
+  };
+
+  const createAnswer = async (offer) => {
+    await Peer.setRemoteDescription(offer);
+    const answer = await Peer.createAnswer();
+    await Peer.setLocalDescription(answer); //ye basically khud ka sdp hai jo dusre wale ne store kara
+    return answer;
+  };
+
+  const setRemoteAns = async (ans) => {
+    await Peer.setRemoteDescription(ans);
+  };
+
+  const sendStream = async (stream) => {
+    const tracks = stream.getTracks();
+    for (const track of tracks) {
+      Peer.addTrack(track, stream);
     }
+  };
 
-    const createAnswer = async (offer)=>{
-        await Peer.setRemoteDescription(offer);
-        const answer = await Peer.createAnswer();
-        await Peer.setLocalDescription(answer);//ye basically khud ka sdp hai jo dusre wale ne store kara
-        return answer;
-    }
+  const handleTrackEvent = useCallback((ev)=>{
+        const streams = ev.streams;
+        setRemoteStream(streams[0])
+    },[])
 
-    const setRemoteAns = async (ans)=>{
-        await Peer.setRemoteDescription(ans);
-    }
 
-    const sendStream = async ( stream )=>{
-const tracks = stream.getTracks();
-for (const track of tracks ){
-    Peer.addTrack(track, stream);
+  useEffect(()=>{
+Peer.addEventListener('track',handleTrackEvent)
+
+return ()=>{ 
+    Peer.removeEventListener('track',handleTrackEvent)
 }
-    }
+},[Peer,handleTrackEvent])
+  
 
-return <PeerContext.Provider value={{Peer, createOffer , createAnswer, setRemoteAns,  sendStream}}>
-    {props.children}
-</PeerContext.Provider>
-}
+  return (
+    <PeerContext.Provider
+      value={{ Peer, createOffer, createAnswer, setRemoteAns, sendStream, remoteStream }}
+    >
+      {props.children}
+    </PeerContext.Provider>
+  );
+};
